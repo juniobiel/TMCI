d73db0dc5f8ee7d4fc9ced5eef03e3ffc1c2e343
Gabriel Júnio <gabriel.junio@rocketmail.com>
2020-06-02 23:05:15 -0300

    Add build

---
+++ build/precache-manifest.9aed379acc00bb2a9815842803f5bfbb.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "154a97137249168890bdc552557ea7e3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bc8be80cbe917d01c0a6",
+    "url": "/static/css/2.d34346ea.chunk.css"
+  },
+  {
+    "revision": "cc9f6d6101773be8bcf3",
+    "url": "/static/css/main.ad9bd6b9.chunk.css"
+  },
+  {
+    "revision": "bc8be80cbe917d01c0a6",
+    "url": "/static/js/2.c6aad4ba.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.c6aad4ba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cc9f6d6101773be8bcf3",
+    "url": "/static/js/main.d688ba38.chunk.js"
+  },
+  {
+    "revision": "055e94f6602794288cc3",
+    "url": "/static/js/runtime-main.1f1f19a3.js"
+  },
+  {
+    "revision": "fb20b3f5b165feb0c2e36af7341147e1",
+    "url": "/static/media/Logomarca_TMCI.fb20b3f5.png"
+  }
+]);

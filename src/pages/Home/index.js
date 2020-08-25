09ce1cf12e8011cd309960236051deef56295940
Gabriel Junio <gabriel.junio@rocketmail.com>
2020-08-25 16:43:53 -0300

    landing page da homepage feita

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 
 import logo from '../../assets/images/logomarca.png';
+import seta from '../../assets/icons/setaIcon.svg';
 
 import './styles.css';
 
@@ -8,21 +10,36 @@ function Home(){
     return(
         <div id="container">
             <section id="logo-container">
-                <img src={logo} alt=""/>
+                <img src={logo} alt="Logomarca"/>
             </section>
             <section id="menu-container">
                 <div id="menu-title">
                     <h1>Serviços de qualidade!</h1>
                 </div>
-                <div id="about">
-                    <h3>Sobre a TMCI</h3>
-                </div>
-                <div id="jobs">
-                    <h3>Nossos Serviços</h3>
-                </div>
-                <div id="contact">
-                    <h3>Contato</h3>
-                </div>
+                <Link to="">
+                    <div className="menu-item">
+
+                            <h3>Sobre a TMCI</h3>
+                            <img src={seta} alt="Icone seta" /> 
+                    </div>
+                </Link>
+
+                <Link to="">
+                    <div className="menu-item">
+                        
+                            <h3>Nossos Serviços</h3>
+                            <img src={seta} alt="Icone seta" /> 
+                                        
+                    </div>
+                </Link>
+                <Link to="">
+                    <div className="menu-item">
+
+                        <h3>Contato</h3>
+                        <img src={seta} alt="Icone seta" /> 
+                    
+                    </div>
+                </Link>
             </section>
         </div>
     );

9b5735e9bb3782cfc545a1960fded478974c7ca6
Gabriel Junio <gabriel.junio@rocketmail.com>
2020-10-08 11:24:23 -0300

    novo component para descriçao de serviços

--- src/pages/OurServices/index.js
+++ src/pages/OurServices/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {FcDownload} from 'react-icons/fc';
 
 import Header from '../../components/Header';
+import ServiceArea from '../../components/ServiceArea';
 
 import './styles.css';
 
@@ -9,6 +10,41 @@ function Services(){
     return(
         <div id="services-container">
             <Header title="Nossos Serviços" nav1="Sobre a TMCI" nav2="Contato" />
+            <section className="thumbnail">
+                <h1>Baixe a cartilha de serviços</h1>
+                <FcDownload size={100}/>
+            </section>
+
+            <ServiceArea 
+            title="Sistemas Elétricos" 
+            p1="Instalação e manutenção em sistemas elétricos em geral" 
+            p2="Fiação, cabeamento, estação de energia, quadros elétricos, etc."
+            />
+
+            <ServiceArea 
+            title="Sistemas de Ar" 
+            p1="Instalação, manutenção e limpeza em sistemas de ar" 
+            p2="Dutos, ar-condicionado, ventilação, entre outros."
+            />
+
+            <ServiceArea 
+            title="Sistemas de Segurança" 
+            p1="Instalação e manutenção em sistemas de segurança" 
+            p2="Sistemas CFTV, câmeras, vigilância e mais."
+            />
+
+            <ServiceArea 
+            title="Sistemas de Rede e Voz" 
+            p1="Instalação e manutenção em sistemas de redes e telefonia" 
+            p2="Fiação, cabeamento, raques, infraestrutura, intranet, etc."
+            />
+
+            <ServiceArea 
+            title="Sistemas Projetos e Obras" 
+            p1="Projetar e construir" 
+            p2="Construção do início ao acabamento, pintura, instalação, demolição, adequações de construções, e muito mais."
+            />
+            
         </div>
     );
 }
